9a7821ee38de9a38c57df3255907dc23d5a61c8c
LookForward <teterin1vl@gmail.com>
2018-03-22 16:36:40 +0900

    fix

--- components/navbar.php
+++ components/navbar.php
@@ -1,18 +1,20 @@
-<?php
-  $filename = basename($_SERVER["SCRIPT_FILENAME"], '.php');
-?>
+<?php $filename = basename($_SERVER["SCRIPT_FILENAME"], '.php'); ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Smart</a>
+	<a class="navbar-brand" href="#">Smart</a>
 
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item <?= ($filename == 'index') ? 'active' : '' ?>">
-        <a class="nav-link" href="/">Index</a>
-      </li>
-      <li class="nav-item <?= ($filename == 'test') ? 'active' : '' ?>">
-        <a class="nav-link" href="/test.php">Test</a>
-      </li>
-    </ul>
-  </div>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbar">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item <?= ($filename == 'index') ? 'active' : '' ?>">
+				<a class="nav-link" href="/">Index</a>
+			</li>
+			<li class="nav-item <?= ($filename == 'test') ? 'active' : '' ?>">
+				<a class="nav-link" href="/test.php">Test</a>
+			</li>
+		</ul>
+	</div>
 </nav>
